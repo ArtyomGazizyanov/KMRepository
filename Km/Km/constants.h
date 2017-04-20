d320778f764ea8ddffc4d724f0feaffff75f6a49
ArtyomGazizyanov <Artyomgad@gmail.com>
2017-04-20 20:04:56 +0300

    Добавил А*
    добавил а*

---
+++ Km/Km/constants.h
@@ -0,0 +1,14 @@
+#pragma once
+/*
+const std::map<size_t, std::pair<size_t, size_t>> Constant::EIGHT_GAME::WIN_POSITIONS =
+{
+	{ 0,{ 0, 0 } },
+	{ 1,{ 1, 0 } },
+	{ 2,{ 2, 0 } },
+	{ 3,{ 0, 1 } },
+	{ 4,{ 1, 1 } },
+	{ 5,{ 2, 1 } },
+	{ 6,{ 0, 2 } },
+	{ 7,{ 1, 2 } },
+	{ 8,{ 2, 2 } }
+};*/
